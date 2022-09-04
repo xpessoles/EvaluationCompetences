53e0dd14b937302903ae515bd789dbb8a145b3c6
Xavier Pessoles <xpessoles.ptsi@free.fr>
2022-09-04 09:42:01 +0200

    Update ajout_competences.py

--- scripts/ajout_competences.py
+++ scripts/ajout_competences.py
@@ -8,9 +8,9 @@ Created on Wed Dec 15 22:03:05 2021
 dossier_comp = "Competences"
 fichier_comp = "Competences_PCSI_PSI.xlsx"
 onglet_comp  = "Competences"
-filiere = 'PCSI-PSI'
+filiere = 'PCSI-PSI' # Pour le choix du programme
 discipline = "SII"
-bdd = "BDD_Evaluation.db"
+bdd = "BDD_2022_2023.db"
 
 ## Import de bibliothèques
 from evaluation.fonctions import read_file_competences,ajout_competences_bdd
