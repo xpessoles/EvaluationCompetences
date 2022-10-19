1e474d24ff4073ff02acbb7c36557572310dcb24
Xavier Pessoles <xpessoles.ptsi@free.fr>
2022-10-19 22:22:17 +0200

    MAJ

--- scripts/ajout_eleves.py
+++ scripts/ajout_eleves.py
@@ -6,8 +6,8 @@ Created on Wed Dec 15 22:03:05 2021
 """
 ## Paramètres 
 dossier_eleve = "Competences"
-fichier_eleve = "Eleves_MPSI1_2022_2023.xlsx"#"Eleves_PSIe_2022_2023.xlsx"
-classe = "MPSI1"#'PSIe'
+fichier_eleve = "Eleves_PTSI2_2022_2023.xlsx"#"Eleves_PSIe_2022_2023.xlsx"
+classe = "PTSI2"#"MPSI1"#'PSIe'
 annee = "2023" # Année de passage du concours
 bdd = "BDD_2022_2023.db"
     
