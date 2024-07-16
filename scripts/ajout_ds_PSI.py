7d5862675de64eec0b350d42567adfdbf7fc3d20
Xavier Pessoles <xpessoles.ptsi@free.fr>
2024-07-16 20:50:04 +0200

    Update ajout_ds_PSI.py

--- scripts/ajout_ds_PSI.py
+++ scripts/ajout_ds_PSI.py
@@ -39,21 +39,21 @@ ord_origine = 0
 evaluation = Evaluation(classe,annee,type_eval,num_eval,date_eval)
 
 # # On ajoute l'EVAL à la BDD
-add_evaluation_bdd(evaluation,bdd)
-
-# # On lit le bareme
-bareme = read_bareme(dossier_notes, fichier_notes, evaluation,bdd)
-
-# # On ajoute le bareme a la BDD
-add_bareme_bdd(bareme,filiere, bdd)
-
-# # On ajoute les notes de chacun des élèves dans la base de donnée
-notes = read_notes(dossier_notes, fichier_notes, evaluation, bdd)
-add_notes_bdd(notes,evaluation,bareme,bdd)
-
-# # # Génération des bilans indivisualisés
-print("Génération bilan indiv")
-generation_bilan_eval_indiv(classe,annee,filiere,evaluation,bdd,COEF_DS,ord_origine,ext)
+# add_evaluation_bdd(evaluation,bdd)
+#
+# # # On lit le bareme
+# bareme = read_bareme(dossier_notes, fichier_notes, evaluation,bdd)
+#
+# # # On ajoute le bareme a la BDD
+# add_bareme_bdd(bareme,filiere, bdd)
+#
+# # # On ajoute les notes de chacun des élèves dans la base de donnée
+# notes = read_notes(dossier_notes, fichier_notes, evaluation, bdd)
+# add_notes_bdd(notes,evaluation,bareme,bdd)
+#
+# # # # Génération des bilans indivisualisés
+# print("Génération bilan indiv")
+# generation_bilan_eval_indiv(classe,annee,filiere,evaluation,bdd,COEF_DS,ord_origine,ext)
 
 # # Génération du bilan de compétences
 #eleves = get_eleves(classe,annee,bdd)
