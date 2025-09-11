bef92499f8b6772f33fe551b0b66d1712863cc81
Xavier Pessoles <xpessoles.ptsi@free.fr>
2025-09-11 15:53:17 +0200

    MAJ

--- scripts/ajout_eleves.py
+++ scripts/ajout_eleves.py
@@ -5,15 +5,16 @@ Created on Wed Dec 15 22:03:05 2021
 @author: Xavier Pessoles
 """
 ## Paramètres
-dossier_eleve = "Competences"
+dossier_eleve = "2025_2026"
 #fichier_eleve = "Eleves_PTSI1_2022_2023.xlsx"
 #"Eleves_MP_2022_2023.xlsx"
-fichier_eleve ="Eleves_MP_2024_2025.xlsx"
+#fichier_eleve ="Eleves_MP_2024_2025.xlsx"
+fichier_eleve ="Eleves_PSIe_2025_2026.xlsx"
 #"Eleves_PTSI1_2023_2024.xlsx"
 #"Eleves_PTSI2_2023_2024.xlsx"#"Eleves_MP_2023_2024.xlsx"#
-classe = "MP"#PSIe"#PTSI1"#PTSI2"#"MPSI1"#'PSIe'
-annee = "2025" # Année de passage du concours des spés
-bdd = "2024_2025_BDD.db"
+classe = "PSIe"#PSIe"#PTSI1"#PTSI2"#"MPSI1"#'PSIe'
+annee = "2026" # Année de passage du concours des spés
+bdd = "2025_2026_BDD.db"
 
 from evaluation.fonctions import read_file_eleves,ajout_eleves_bdd
 
